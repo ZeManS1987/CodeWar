7491739504b4859ec57cfb72133fdf6574a6aff5
ZeManS1987 <93866994+ZeManS1987@users.noreply.github.com>
2023-03-05 17:01:40 +0300

    HextoDecimal

---
+++ HextoDecimal/Kata.java
@@ -0,0 +1,10 @@
+package HextoDecimal;
+
+public class Kata {
+
+    public static int hexToDec(final String hexString) {
+        // Your code here
+        return Integer.parseInt(hexString, 16);
+    }
+
+}
